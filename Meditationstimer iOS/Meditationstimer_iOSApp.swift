27e526274047b1cdeb276494c8f98dfc93aac34c
Henning Emmrich <henning.emmrich@gmail.com>
2025-09-01 22:49:08 +0200

    Stabiler Stand mit beiden Plattformen

---
+++ Meditationstimer iOS/Meditationstimer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditationstimer_iOSApp.swift
+//  Meditationstimer iOS
+//
+//  Created by Henning Emmrich on 01.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Meditationstimer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
