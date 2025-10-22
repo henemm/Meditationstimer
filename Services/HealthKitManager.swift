a3eca4860c7ae4ed7c5d9324cf9a9f26b35c3de8
Henning Emmrich <henning.emmrich@gmail.com>
2025-10-22 22:06:10 +0200

    Remove invalid @AppStorage from HealthKitManager class

--- Services/HealthKitManager.swift
+++ Services/HealthKitManager.swift
@@ -56,7 +56,6 @@ final class HealthKitManager {
     }
 
     private let healthStore = HKHealthStore()
-    @AppStorage("calendarFilterEnabled") private var calendarFilterEnabled: Bool = false
 
     /// Hinweis zu Info.plist:
     /// - NSHealthShareUsageDescription  → Begründung für das LESEN von Health‑Daten (z. B. Herzfrequenz)
